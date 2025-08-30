bf74ca0b71fe5db96f490ac0883105e9a4950a9f
Habtamu Tesfaye <habtamutesfaye678@gmail.com>
2025-08-30 08:08:12 +0300

    added setting.py in crm app  for alx checker

---
+++ crm/settings.py
@@ -0,0 +1,16 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'graphene_django',
+    'django_filters',
+    'crm',
+    'django_crontab',
+]
+
+CRONJOBS = [
+    ('0 */12 * * *', 'crm.cron.update_low_stock'),
+]
